ffe49598f1a41d23adb4ae075a20f21af0c1438b
BARA Anis <40480699+barustnt@users.noreply.github.com>
2018-07-31 11:14:19 +0100

    after adding photo

--- untitled/venv/bb.py
+++ untitled/venv/bb.py
@@ -1,25 +1,11 @@
+from tkinter import *
 import os
-import Tkinter
-import tkMessageBox
+root = Tk()
+theLabel = Label(root, text="Folow the putun please")
+theLabel.pack()
 
-Manager = Tkinter.Tk()
+photo = PhotoImage(file="ind.png")
+label=Label(root, image=photo)
+label.pack()
 
-# Presentation du programme
-
-presentation = Tkinter.Label(Manager, text='Mise en route du programme NetworkManager')
-presentation.pack()
-
-
-# Programme d'execution
-
-def Network():
-    ad = "su root -c /usr/sbin/NetworkManager"
-    com = os.system(ad)
-    tkMessageBox.showinfo("INFO", "Fonctionnement de Network")
-
-
-# Creation du bouton Network
-
-Bouton = Tkinter.Button(Manager, text='NetworkManager', command=Network, bg='yellow', fg='black')
-Bouton.pack()
-Manager.mainloop()
+root.mainloop()
--- untitled/venv/tkinter1.py
+++ untitled/venv/tkinter1.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import os
+import subprocess
+
 root = Tk()
 theLabel = Label(root, text="Folow the putun please")
 theLabel.pack()
@@ -7,11 +9,15 @@ top= Frame(root)
 top.pack()
 bottomFrame =Label(root)
 bottomFrame.pack(side=BOTTOM)
+photo=PhotoImage(file="ind.png")
+theLabel=Label(root, image=photo)
+theLabel.pack()
 def list ():
-    exec= "cmd"
+    exec= "services.msc"
+  # subprocess
     os.system(exec)
 button1=Button(top, text="Record" , fg="red" , command=list)
-button2=Button(top, text="press to enter the name of butun" , fg="blue")
+button2=Button(top, text="press to enter the name of butun" , fg="blue",command=photo)
 button3=Button(bottomFrame, text="end Record" , fg="green" ,command=bottomFrame.quit)
 button1.pack()
 button2.pack()
